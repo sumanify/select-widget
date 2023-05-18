fa87269d0621f510fc550eab511abc0be28056c2
Suman Kharel <sumanify@gmail.com>
2023-05-18 17:15:39 +0300

    Multi Select

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,44 @@
 import React from "react";
-import { SelectWidget } from "./SelectWidget/SelectWidget";
+import { ISelectOption, SelectWidget } from "./SelectWidget/SelectWidget";
 
 const options = [
   { value: "1", label: "One" },
   { value: "2", label: "Two" },
   { value: 3, label: "Three" },
+  { value: 4, label: "Four" },
+  { value: 5, label: "Five" },
+  { value: 6, label: "Six" },
+  { value: 7, label: "Seven" },
+  { value: 8, label: "Eight" },
+  { value: 9, label: "Nine" },
+  { value: 10, label: "Ten" },
 ];
 
-interface ISelectOption {
-  value: number | string;
-  label: string;
-}
-
 function App() {
-  const [value, setValue] = React.useState<ISelectOption | undefined>(
+  const [value1, setValue1] = React.useState<ISelectOption | undefined>(
     options[1]
   );
+
+  const [value2, setValue2] = React.useState<ISelectOption[]>([
+    options[1],
+    options[2],
+  ]);
 
   return (
     <div>
       <SelectWidget
-        onChange={(value) => setValue(value)}
+        onChange={(value) => setValue1(value)}
         options={options}
-        value={value}
+        value={value1}
+      />
+
+      <br />
+
+      <SelectWidget
+        multiple
+        onChange={(value) => setValue2(value)}
+        options={options}
+        value={value2}
       />
     </div>
   );
--- src/SelectWidget/SelectWidget.tsx
+++ src/SelectWidget/SelectWidget.tsx
@@ -1,45 +1,126 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./selectwidget.module.css";
 
-interface ISelectOption {
+export interface ISelectOption {
   value: number | string;
   label: string;
 }
 
-interface ISelectProps {
+interface ISingleSelect {
+  multiple?: false;
   onChange: (value: ISelectOption | undefined) => void;
   value?: ISelectOption;
-  options: ISelectOption[];
 }
 
-export function SelectWidget({ value, onChange, options }: ISelectProps) {
+interface IMultiSelect {
+  multiple: true;
+  onChange: (value: ISelectOption[]) => void;
+  value?: ISelectOption[];
+}
+
+type TSelectProps = {
+  options: ISelectOption[];
+} & (ISingleSelect | IMultiSelect);
+
+export function SelectWidget({
+  multiple,
+  value,
+  onChange,
+  options,
+}: TSelectProps) {
   const [isOpen, setIsOpen] = useState(false);
   const [highlightedIndex, setHighlightedIndex] = useState(0);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   function handleClearOptions() {
-    onChange(undefined);
+    multiple ? onChange([]) : onChange(undefined);
   }
 
   function handleSelectOption(option: ISelectOption) {
-    if (option !== value) onChange(option);
+    if (multiple) {
+      if (value?.includes(option)) {
+        onChange(value?.filter((v) => v !== option));
+      } else {
+        onChange([...(value || []), option]);
+      }
+    } else {
+      if (option !== value) onChange(option);
+    }
   }
 
   function getIsOptionSelected(option: ISelectOption) {
-    return option.value === value?.value;
+    return multiple ? value?.includes(option) : option.value === value?.value;
   }
 
   useEffect(() => {
     if (isOpen) setHighlightedIndex(0);
   }, [isOpen]);
 
+  useEffect(() => {
+    const containerCurrent = containerRef.current;
+    const handler = (e: KeyboardEvent) => {
+      if (e.target !== containerRef.current) return;
+
+      switch (e.code) {
+        case "Enter":
+        case "Space":
+          setIsOpen((prev) => !prev);
+          if (!isOpen) handleSelectOption(options[highlightedIndex]);
+          break;
+
+        case "ArrowUp":
+        case "ArrowDown": {
+          if (!isOpen) {
+            setIsOpen(true);
+            break;
+          }
+
+          const newValue = highlightedIndex + (e.code === "ArrowDown" ? 1 : -1);
+          if (newValue >= 0 && newValue < options.length) {
+            setHighlightedIndex(newValue);
+          }
+          break;
+        }
+
+        case "Escape":
+          setIsOpen(false);
+          break;
+      }
+    };
+
+    containerCurrent?.addEventListener("keydown", handler);
+
+    return () => {
+      containerCurrent?.removeEventListener("keydown", handler);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isOpen, highlightedIndex, options]);
+
   return (
     <div
+      ref={containerRef}
       tabIndex={0}
       className={styles.container}
       onClick={() => setIsOpen((prev) => !prev)}
       onBlur={() => setIsOpen(false)}
     >
-      <span className={styles.value}>{value?.label}</span>
+      <span className={styles.value}>
+        {multiple
+          ? value?.map((v) => (
+              <button
+                key={v.value}
+                className={styles["option-badge"]}
+                onClick={(event) => {
+                  event.stopPropagation();
+                  handleSelectOption(v);
+                }}
+              >
+                {v.label}
+                <span className={styles["remove-btn"]}>&times;</span>
+              </button>
+            ))
+          : value?.label}
+      </span>
       <button
         onClick={(event) => {
           event.stopPropagation();
